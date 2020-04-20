39c55055dfad9b6ad85db01380b3b257a0c7232b
CoooooLer <13939@qq.com>
2020-04-20 17:58:44 +0800

    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,27 +1,56 @@
 //index.js
 //获取应用实例
 const app = getApp()
+import {request} from '../../request/index'
 
 Page({
   data: {
-    // 轮播数组
-    swiperList: []
+    // 轮播 数组
+    swiperList: [],
+    // 导航栏 数组
+    cateList: [],
+    // 楼层 数组
+    floorList: []
   },
   onShow() {
-    console.log(0)
-    console.log(sum(5))
   },
   onLoad: function () {
-    wx.request({
-      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
-      success: (result)=>{
-        console.log(result.data.message)
+    this.getSwiperList();
+    this.getCateList();
+    this.getFloorList();
+  },
+  // 轮播图
+  getSwiperList() {
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'
+    })
+      .then(res => {
         this.setData({
-          swiperList: result.data.message
+          swiperList: res.data.message
         })
-      }
-    });
-    
+      })    
+  },
+  // 导航栏
+  getCateList() {
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'
+    })
+      .then(res => {
+        this.setData({
+          cateList: res.data.message
+        })
+      })    
+  },
+   // 楼层
+   getFloorList() {
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata'
+    })
+      .then(res => {
+        this.setData({
+          floorList: res.data.message
+        })
+      })    
   },
   sum: function(num){
     if(num==1){						
